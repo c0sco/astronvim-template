a470bbf1f5fa69f49461634cea31d70ce42d25ba
Matt Stofko <778087+c0sco@users.noreply.github.com>
2025-03-18 18:43:05 -0700

    Adding addtl. packs

--- lua/community.lua
+++ lua/community.lua
@@ -7,6 +7,18 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+  { import = "astrocommunity.diagnostics.trouble-nvim" },
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.nix" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.sql" },
+  { import = "astrocommunity.pack.terraform" },
+  { import = "astrocommunity.pack.typescript-all-in-one" },
+  { import = "astrocommunity.session.vim-workspace" },
   -- import/override with your plugins folder
 }
