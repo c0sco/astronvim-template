88d62055b304c620d8f6afb46a09797df6962943
Matt Stofko <778087+c0sco@users.noreply.github.com>
2025-03-18 18:42:14 -0700

    Adding awk and ADO

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -11,6 +11,8 @@ return {
     opts = {
       ensure_installed = {
         "lua_ls",
+        "awk_ls",
+        "azure_pipelines_ls",
         -- add more arguments for adding more language servers
       },
     },
